fcc4a3723b80aae980024a4bfdfe4ff2832bcbb7
JestemKamil <84380834+JestemKamil@users.noreply.github.com>
2023-03-11 20:24:25 +0100

    Wersja v1.0 -- glowna funkcjonalnosc

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 const express = require("express")
 const app = express()
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client({
+    checkUpdate: false,
+});
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+})
 
 app.use(express.urlencoded({extended: false}))
 app.set("view engine", "ejs")
+
+app.get("/", (req,res) =>{
+    res.render("index")
+})
+app.post("/login",async (req,res) =>{
+    const token = req.body.token
+    const channelId = req.body.channelId
+    const repeats = req.body.repeats
+    const content = req.body.content
+    try{
+    await client.login(token)
+    console.log(`Zalogowano jako ${client.user.username}`)
+    const kanal = await client.channels.fetch(channelId)
+    for (let index = 0; index < repeats; index++) {
+        await kanal.send(content)
+    }
+    res.redirect("/gotowe")
+    }catch{
+            res.render("error")
+        }
+})
+app.get("/gotowe", (req,res) =>{
+    res.render("done.ejs")
+})
+
+app.listen(3000)
